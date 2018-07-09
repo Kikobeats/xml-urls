1b2039e94cd30d5ac593150d92958fece41871aa
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-07-09 17:15:40 +0200

    Refactor

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const REGEX_URL_XML = /\.xml$/
 
 const isXmlUrl = url => REGEX_URL_XML.test(url)
 
-const walkUrls = async (url, opts) => {
+const xmlUrls = async (url, opts) => {
   const { origin: baseUrl } = new URL(url)
   const { body: html } = await got(url, opts)
   const $ = cheerio.load(html, { xmlMode: true })
@@ -25,13 +25,15 @@ const walkUrls = async (url, opts) => {
     .get()
 
   const iterator = async (set, url) => {
-    if (!isXmlUrl(url)) return new Set([...set, getUrl(baseUrl, url)])
-    const innerUrls = await walkUrls(url, opts)
-    return new Set([...set, ...innerUrls])
+    const urls = isXmlUrl(url)
+      ? await xmlUrls(url, opts)
+      : [getUrl(baseUrl, url)]
+    return new Set([...set, ...urls])
   }
 
   return aigle.reduce(urls, iterator, new Set())
 }
 
-module.exports = async (url, opts) => Array.from(await walkUrls(url, opts))
+module.exports = async (url, opts) => Array.from(await xmlUrls(url, opts))
+
 module.exports.isXmlUrl = isXmlUrl
