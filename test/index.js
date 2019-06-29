0679244e9e5905ab905825b765bd51366e32f8f7
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-06-29 23:26:46 +0200

    test: unify fixtures

--- test/index.js
+++ test/index.js
@@ -3,37 +3,38 @@
 const test = require('ava')
 const xmlUrls = require('..')
 
+/**
+ * tests files at: https://gist.github.com/Kikobeats/317550e76f1cbd399cebe3bddc0c146b
+ */
+const fixtures = {
+  sitemap:
+    'https://gistcdn.githack.com/Kikobeats/317550e76f1cbd399cebe3bddc0c146b/raw/7d9508f4c1284efe44f9845ad8e8a748d12a3365/sitemap.xml',
+  sitemaWithDuplicates:
+    'https://gistcdn.githack.com/Kikobeats/317550e76f1cbd399cebe3bddc0c146b/raw/7df528dcbf0489ee51b50df648f5e5ea010e10ae/sitemap_with_duplicates.xml',
+  sitemapOfSitemaps:
+    'https://gistcdn.githack.com/Kikobeats/317550e76f1cbd399cebe3bddc0c146b/raw/7df528dcbf0489ee51b50df648f5e5ea010e10ae/sitemap_of_sitemaps.xml'
+}
+
 test('Get all URLs from a plain sitemap', async t => {
-  const urls = await xmlUrls(
-    'https://rawgit.com/Kikobeats/317550e76f1cbd399cebe3bddc0c146b/raw/40a311a2930d19755332818ee4a8a14e59b6ef04/sitemap.xml',
-    { prerender: false }
-  )
+  const urls = await xmlUrls(fixtures.sitemap, { prerender: false })
   t.snapshot(urls)
 })
 
 test('Remove duplicates', async t => {
-  const urls = await xmlUrls(
-    'https://gist.githubusercontent.com/Kikobeats/2df83a67729397032c3623974966a112/raw/87ba48da0ed5123ac50c1cbd0d2d0b473dccd10a/gistfile1.txt',
-    { prerender: false }
-  )
+  const urls = await xmlUrls(fixtures.sitemaWithDuplicates, {
+    prerender: false
+  })
   t.snapshot(urls)
 })
 
 test('Get all URLs from more than one sitemap', async t => {
-  const urls = await xmlUrls(
-    [
-      'https://rawgit.com/Kikobeats/317550e76f1cbd399cebe3bddc0c146b/raw/40a311a2930d19755332818ee4a8a14e59b6ef04/sitemap.xml',
-      'https://rawgit.com/Kikobeats/317550e76f1cbd399cebe3bddc0c146b/raw/40a311a2930d19755332818ee4a8a14e59b6ef04/sitemap.xml'
-    ],
-    { prerender: false }
-  )
+  const urls = await xmlUrls([fixtures.sitemap, fixtures.sitemap], {
+    prerender: false
+  })
   t.snapshot(urls)
 })
 
 test('Get all URLs from a sitemap of sitemaps', async t => {
-  const urls = await xmlUrls(
-    'https://rawgit.com/Kikobeats/317550e76f1cbd399cebe3bddc0c146b/raw/7d9508f4c1284efe44f9845ad8e8a748d12a3365/sitemap_of_sitemaps.xml',
-    { prerender: false }
-  )
+  const urls = await xmlUrls(fixtures.sitemapOfSitemaps, { prerender: false })
   t.snapshot(urls)
 })
