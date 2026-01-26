ec9a451db3fb27381d091e25a83d922a2ba9c2b8
Kiko Beats <josefrancisco.verdu@gmail.com>
2026-01-26 17:34:18 +0100

    test: update snapshot

--- test/index.js
+++ test/index.js
@@ -3,18 +3,7 @@
 const test = require('ava')
 const xmlUrls = require('..')
 
-const { getBrowserless } = require('./util')
-/**
- * tests files at: https://gist.github.com/Kikobeats/317550e76f1cbd399cebe3bddc0c146b
- */
-const fixtures = {
-  sitemap:
-    'https://gistcdn.githack.com/Kikobeats/317550e76f1cbd399cebe3bddc0c146b/raw/7d9508f4c1284efe44f9845ad8e8a748d12a3365/sitemap.xml',
-  sitemaWithDuplicates:
-    'https://gistcdn.githack.com/Kikobeats/317550e76f1cbd399cebe3bddc0c146b/raw/7df528dcbf0489ee51b50df648f5e5ea010e10ae/sitemap_with_duplicates.xml',
-  sitemapOfSitemaps:
-    'https://gistcdn.githack.com/Kikobeats/317550e76f1cbd399cebe3bddc0c146b/raw/7df528dcbf0489ee51b50df648f5e5ea010e10ae/sitemap_of_sitemaps.xml'
-}
+const { getBrowserless, fixtures } = require('./util')
 
 test('Get all URLs from a plain sitemap', async t => {
   const urls = await xmlUrls(fixtures.sitemap, { prerender: false, getBrowserless })
@@ -39,5 +28,6 @@ test('Get all URLs from more than one sitemap', async t => {
 
 test('Get all URLs from a sitemap of sitemaps', async t => {
   const urls = await xmlUrls(fixtures.sitemapOfSitemaps, { prerender: false, getBrowserless })
+  console.log('urls', urls)
   t.snapshot(urls)
 })
--- test/util.js
+++ test/util.js
@@ -6,6 +6,19 @@ const { onExit } = require('signal-exit')
 const browserlessFactory = createBrowserless()
 onExit(browserlessFactory.close)
 
+/**
+ * tests files at: https://gist.github.com/Kikobeats/317550e76f1cbd399cebe3bddc0c146b
+ */
+const fixtures = {
+  sitemap:
+    'https://gist.githubusercontent.com/Kikobeats/317550e76f1cbd399cebe3bddc0c146b/raw/023496c5bb402df8854c995eec4f6db2a8f131e3/sitemap.xml',
+  sitemaWithDuplicates:
+    'https://gist.githubusercontent.com/Kikobeats/317550e76f1cbd399cebe3bddc0c146b/raw/023496c5bb402df8854c995eec4f6db2a8f131e3/sitemap_with_duplicates.xml',
+  sitemapOfSitemaps:
+    'https://gist.githubusercontent.com/Kikobeats/317550e76f1cbd399cebe3bddc0c146b/raw/023496c5bb402df8854c995eec4f6db2a8f131e3/sitemap_of_sitemaps.xml'
+}
+
 module.exports = {
+  fixtures,
   getBrowserless: () => browserlessFactory
 }
