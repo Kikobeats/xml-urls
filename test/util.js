518a3854c4cc57f05fed857b413b01f8e3e0dfc8
Kiko Beats <josefrancisco.verdu@gmail.com>
2026-03-23 10:20:59 +0100

    test: update snapshot

--- test/util.js
+++ test/util.js
@@ -11,11 +11,11 @@ onExit(browserlessFactory.close)
  */
 const fixtures = {
   sitemap:
-    'https://gist.githubusercontent.com/Kikobeats/317550e76f1cbd399cebe3bddc0c146b/raw/023496c5bb402df8854c995eec4f6db2a8f131e3/sitemap.xml',
+    'https://gist.githubusercontent.com/Kikobeats/317550e76f1cbd399cebe3bddc0c146b/raw/69f37258c61cc8114d25a86238bca572d5f81c30/sitemap.xml',
   sitemaWithDuplicates:
-    'https://gist.githubusercontent.com/Kikobeats/317550e76f1cbd399cebe3bddc0c146b/raw/023496c5bb402df8854c995eec4f6db2a8f131e3/sitemap_with_duplicates.xml',
+    'https://gist.githubusercontent.com/Kikobeats/317550e76f1cbd399cebe3bddc0c146b/raw/69f37258c61cc8114d25a86238bca572d5f81c30/sitemap_with_duplicates.xml',
   sitemapOfSitemaps:
-    'https://gist.githubusercontent.com/Kikobeats/317550e76f1cbd399cebe3bddc0c146b/raw/023496c5bb402df8854c995eec4f6db2a8f131e3/sitemap_of_sitemaps.xml'
+    'https://gist.githubusercontent.com/Kikobeats/317550e76f1cbd399cebe3bddc0c146b/raw/69f37258c61cc8114d25a86238bca572d5f81c30/sitemap_of_sitemaps.xml'
 }
 
 module.exports = {
